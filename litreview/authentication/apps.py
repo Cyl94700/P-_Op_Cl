4d73a7f170b6ed37d7484e57834306f2a7c00135
Cyl94700 <cyrilcouybes@gmail.com>
2022-10-14 17:30:07 +0200

    models authentication et correctifs

--- litreview/authentication/apps.py
+++ litreview/authentication/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'athentication'
+    name = 'authentication'
